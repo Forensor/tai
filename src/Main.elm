5e74f83d33faa72cb4bf346d4b244a1ceb8358d6
Álvaro Sánchez <forensor@hotmail.com>
2022-04-05 16:26:10 +0200

    Front finished

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,11 @@
 module Main exposing (..)
 
-import Array exposing (Array)
+import Array exposing (Array, fromList, get, repeat, set)
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (id, src)
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class, classList, id)
 import Html.Events exposing (onClick)
-import Html exposing (button)
-import Html.Attributes exposing (classList)
-import Html.Attributes exposing (class)
+import List exposing (all, any, map)
 
 
 
@@ -63,11 +61,24 @@ type alias Coord =
 
 init : Model
 init =
-    { board = Array.repeat 3 <| Array.fromList [ E, E, E ]
+    { board = repeat 3 <| fromList [ E, E, E ]
     , turn = Crosses
     , crosses = Human
     , noughts = Human
     }
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { view = view
+        , init = init
+        , update = update
+        }
+
+
+
+-- Helpers --------------------------------------------------------------------
 
 
 nextTurn : Team -> Team
@@ -80,42 +91,24 @@ nextTurn turn =
             Crosses
 
 
-move : Piece -> Coord -> Model -> Model
-move piece coord model =
-    let
-        newBoard =
-            placePiece coord model.board piece
-    in
-    case getPiece coord model.board of
-        Just E ->
-            { model | board = newBoard, turn = nextTurn model.turn }
+showTeam : Team -> String
+showTeam team =
+    case team of
+        Crosses ->
+            "Crosses"
 
-        _ ->
-            model
+        Noughts ->
+            "Noughts"
 
 
-placePiece : Coord -> Board -> Piece -> Board
-placePiece ( row, col ) board piece =
-    case Array.get row board of
-        Just ps ->
-            Array.set row (Array.set col piece ps) board
+teamToPiece : Team -> Piece
+teamToPiece team =
+    case team of
+        Crosses ->
+            X
 
-        Nothing ->
-            board
-
-
-
--- Helpers --------------------------------------------------------------------
-
-
-getPiece : Coord -> Board -> Maybe Piece
-getPiece ( row, col ) board =
-    case Array.get row board of
-        Just ps ->
-            Array.get col ps
-
-        Nothing ->
-            Nothing
+        Noughts ->
+            O
 
 
 gameTypeToModel : GameType -> Model
@@ -146,70 +139,46 @@ allCoords =
 
 
 
--- Update ---------------------------------------------------------------------
+-- Game manipulation ----------------------------------------------------------
 
 
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        ChangeGameType gameType ->
-            gameTypeToModel gameType
+move : Piece -> Coord -> Model -> Model
+move piece coord model =
+    let
+        newBoard =
+            placePiece coord model.board piece
+    in
+    case getPiece coord model.board of
+        Just E ->
+            { model | board = newBoard, turn = nextTurn model.turn }
 
-        Move piece coord ->
-            move piece coord model
-
-
-
--- Views ----------------------------------------------------------------------
-
-
-teamToPiece : Team -> Piece
-teamToPiece team =
-    case team of
-        Crosses ->
-            X
-
-        Noughts ->
-            O
+        _ ->
+            model
 
 
-view : Model -> Html Msg
-view model =
-    div [ id "app" ]
-        [ viewPanel
-        , viewBoard model
-        , viewInfo model
-        ]
-
-viewPanel : Html Msg
-viewPanel = 
-    div [ id "panel" ]
-        [ button [onClick <| ChangeGameType HumanVsHuman]
-            [ text "Human vs Human" ]
-        , button [onClick <| ChangeGameType HumanVsTai]
-            [ text "Human vs Tai" ]
-        , button [ onClick <| ChangeGameType TaiVsHuman]
-            [ text "Tai vs Human" ]
-        ]
+getPiece : Coord -> Board -> Maybe Piece
+getPiece ( row, col ) board =
+    get row board |> Maybe.andThen (get col)
 
 
-viewInfo : Model -> Html Msg
-viewInfo model =
-    div [id "info"] ([] ++
-    if gameWon model then
-        [text (showTeam (nextTurn model.turn) ++ " won!")]
+placePiece : Coord -> Board -> Piece -> Board
+placePiece ( row, col ) board piece =
+    case get row board of
+        Just ps ->
+            set row (set col piece ps) board
 
-    else if noMoreMoves model then
-        [text "Draw"]
+        Nothing ->
+            board
 
-    else
-        [text (showTeam model.turn ++ "' turn")])
 
-showTeam : Team -> String
-showTeam team =
-    case team of
-        Crosses -> "Crosses"
-        Noughts -> "Noughts"
+
+-- Game control ---------------------------------------------------------------
+
+
+noMoreMoves : Model -> Bool
+noMoreMoves model =
+    all ((/=) (Just E)) <| map (\c -> getPiece c model.board) allCoords
+
 
 gameWon : Model -> Bool
 gameWon model =
@@ -228,43 +197,87 @@ gameWon model =
             , [ ( 0, 2 ), ( 1, 1 ), ( 2, 0 ) ]
             ]
     in
-    List.any ((==) True) <| List.map (\cs -> inline (List.map (\c -> getPiece c model.board) cs)) possibilities
+    any ((==) True) <|
+        map (\cs -> inline (map (\c -> getPiece c model.board) cs)) possibilities
 
 
-noMoreMoves : Model -> Bool
-noMoreMoves model =
-    List.all ((/=) (Just E)) <| List.map (\c -> getPiece c model.board) allCoords
+
+-- Update ---------------------------------------------------------------------
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        ChangeGameType gameType ->
+            gameTypeToModel gameType
+
+        Move piece coord ->
+            move piece coord model
+
+
+
+-- Views ----------------------------------------------------------------------
+
+
+view : Model -> Html Msg
+view model =
+    div [ id "app" ]
+        [ viewPanel
+        , viewBoard model
+        , viewInfo model
+        ]
+
+
+viewPanel : Html Msg
+viewPanel =
+    div [ id "panel" ]
+        [ button [ onClick <| ChangeGameType HumanVsHuman ]
+            [ text "Human vs Human" ]
+        , button [ onClick <| ChangeGameType HumanVsTai ]
+            [ text "Human vs Tai" ]
+        , button [ onClick <| ChangeGameType TaiVsHuman ]
+            [ text "Tai vs Human" ]
+        ]
+
+
+viewInfo : Model -> Html Msg
+viewInfo model =
+    div [ id "info" ]
+        ([]
+            ++ (if gameWon model then
+                    [ text (showTeam (nextTurn model.turn) ++ " won!") ]
+
+                else if noMoreMoves model then
+                    [ text "Draw" ]
+
+                else
+                    [ text (showTeam model.turn ++ "' turn") ]
+               )
+        )
 
 
 viewBoard : Model -> Html Msg
 viewBoard model =
-    div [id "board"] (List.map (viewPiece model) allCoords)
+    div [ id "board" ] (map (viewPiece model) allCoords)
 
 
 viewPiece : Model -> Coord -> Html Msg
 viewPiece model coord =
     case getPiece coord model.board of
         Just X ->
-            div [classList [("square", True),("x", True)]] [ ]
+            div [ classList [ ( "square", True ), ( "x", True ) ] ] []
 
         Just O ->
-            div [classList [("square", True),("o", True)]] [ ]
+            div [ classList [ ( "square", True ), ( "o", True ) ] ] []
 
         _ ->
-            div (class "square" ::
-                (if gameWon model then
-                    []
+            div
+                (class "square"
+                    :: (if gameWon model then
+                            []
 
-                 else
-                    [ onClick <| Move (teamToPiece model.turn) coord ]
-                ))
-                [ ]
-
-
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { view = view
-        , init = init
-        , update = update
-        }
+                        else
+                            [ onClick <| Move (teamToPiece model.turn) coord ]
+                       )
+                )
+                []
